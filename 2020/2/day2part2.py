e318d0354ebe44a459a6df096c8890d18ee7ae43
Steve Listemaa <steve@MacBook-Pro-2.local>
2020-12-02 00:41:17 -0500

    Added Day 2

---
+++ 2020/2/day2part2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+inputfile = "day2input.txt"
+
+if __name__ == '__main__':
+
+    fincount = 0
+    file = open(inputfile,"r")
+    lines = file.readlines()
+    for line in lines:
+        entries = line.split()
+        (mymin,mymax) = entries[0].split('-')
+        mychar = entries[1].replace(':','')
+        mypass = entries[2]
+        if mypass[int(mymin)-1] == mychar or mypass[int(mymax)-1] == mychar:
+            if mypass[int(mymin)-1] == mychar and mypass[int(mymax)-1] == mychar:
+                junk = 1
+            else:
+                fincount = fincount + 1
+    print("FINAL "+str(fincount))
