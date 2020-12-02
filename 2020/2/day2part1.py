f2ad4c4a3170092f6e0af4f6733ece8715908a06
Steve Listemaa <steve@MacBook-Pro-2.local>
2020-12-02 00:38:39 -0500

    Added Day 2

---
+++ 2020/2/day2part1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+inputfile = "day2input.txt"
+
+if __name__ == '__main__':
+
+    fincount = 0
+    file = open(inputfile,"r")
+    lines = file.readlines()
+    for line in lines:
+        entries = line.split()
+        (mymin,mymax) = entries[0].split('-')
+        mychar = entries[1].replace(':','')
+        mycount = entries[2].count(mychar)
+        if mycount >= int(mymin) and mycount <= int(mymax):
+            fincount = fincount + 1
+    print("Final count: " + str(fincount))
